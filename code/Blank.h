9bf3aedb4e91f2a1f8c9127ad550981bf6a1a647
isaacmichaan <46087877+isaacmichaan@users.noreply.github.com>
2019-11-14 04:31:08 +0200

    Update Blank.h

--- code/Blank.h
+++ code/Blank.h
@@ -7,12 +7,12 @@ class Guard;
 class Blank : public StaticObject
 {
 public:
-	Blank();
-	Blank(const sf::Vector2f &, const sf::Vector2f &);
+	//Blank();
+	using StaticObject::StaticObject;
+	//Blank(const sf::Vector2f &, const sf::Vector2f &);
 	~Blank();
 	virtual void draw(sf::RenderWindow & m_window) { m_window.draw(m_rect); }
 	virtual void collide(Robot &);
-	virtual void collide(Guard &);
+	virtual bool collide(Guard &);
 	void setPosition(MoveObject &, sf::Vector2f oPos);
 };
-
